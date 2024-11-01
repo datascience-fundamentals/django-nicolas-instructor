43b98fc446c67df5a92fcb01a41d36cf52307485
rolandoandre123 <rbardalespriale@gmail.com>
2024-11-01 14:54:38 -0500

    add : interfaz listado de producto y busqueda por i

--- productos/models.py
+++ productos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,9 +7,22 @@ from django.db import models
 class Categoria(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return str(self.nombre)
+
+# CASCADE: elimina los productos asociados a la categoria
+# PROTECT: lanza un error al intentar eliminar una categoria
+# RESTRICT: eliminar solo los productos que no tiene categoria
+# SET_NULL: asgina el valor NULL a la columna categoria de aquellos productos que le pertenecian
+# SET_DEFAULT: asigna el valor por defecto establecido para la columna categoria de aquellos productos que le pertenecian
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     stock = models.IntegerField()
     puntaje = models.FloatField()
     categoria = models.ForeignKey(to=Categoria, on_delete=models.CASCADE)
+    creado_en = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.nombre)
--- productos/views.py
+++ productos/views.py
@@ -1,8 +1,41 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .forms import ProductoForm
+from .models import Producto
 
 # Create your views here.
+# /productos
 
 
 def index(request):
-    return HttpResponse('Hola mundo!')
+    productos = Producto.objects.all()
+    # productos = list(Producto.objects.all().values())
+    # productos = Producto.objects.filter(puntaje__gte=5)
+    # productos = Producto.objects.get(pk=1)
+    # return HttpResponse(productos[0].nombre)
+    # return JsonResponse(productos, safe=False)
+    return render(
+        request,
+        "index.html",
+        context={"productos": productos},
+    )
+
+
+def detalle(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    # producto = Producto.objects.get(id=producto_id)
+    return render(
+        request,
+        "detalle.html",
+        context={"producto": producto},
+    )
+
+
+def formulario(request):
+    form = ProductoForm()
+    return render(
+        request,
+        "producto_form.html",
+        context={"form": form}
+    )
